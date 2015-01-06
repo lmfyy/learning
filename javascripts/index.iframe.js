085951fb7fa00475b21a6dfb826dd122db1a6032
lmf <21679183@qq.com>
2015-01-07 04:36:02 +0800

    提交所有更改与新建

---
+++ javascripts/index.iframe.js
@@ -0,0 +1,6 @@
+//首页iframe框架自动调整高度，目前未实现，待续。
+function auto_height(){
+	document.getElementById("mainFrame").style.height=document.getElementById("main").style.height;
+	//parent.document.all("mainFrame").style.height=document.body.scrollHeight; 
+	//parent.document.all("mainFrame").style.width=document.body.scrollWidth; 
+}
